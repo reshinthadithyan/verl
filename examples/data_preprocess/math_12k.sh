5d922e74b22b2e14ce3ba6d794fddd10f52ed2a6
Reshinth Adithyan <reshinth@ip-10-0-165-214.us-west-2.compute.internal>
2025-11-18 05:05:29 +0000

    Fixing paths

--- examples/data_preprocess/math_12k.sh
+++ examples/data_preprocess/math_12k.sh
@@ -3,7 +3,7 @@ set -x
 stage_splits="30,40,30"
 stage_noises="0.0,1.0,1.0"
 
-python math_12k_srt.py \
+python3 examples/data_preprocess/math_12k_srt.py \
     --local_dir ./data/math_12k \
     --add_self_consistency_labels \
     --stage_noises=$stage_noises \
